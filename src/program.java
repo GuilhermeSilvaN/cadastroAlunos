98e23afa2fa65cf764398c24d8526d80e23e94d3
GuilhermeSilvaN <guilhermesilvaalvescardoso@gmail.com>
2025-09-14 12:21:22 -0300

    add looping

--- src/program.java
+++ src/program.java
@@ -2,5 +2,8 @@
 public class program {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }    
 }
